858fc053775a6706b4c256a74dff79db25f8e135
ZZhuZi <3417912281@qq.com>
2019-06-11 21:00:47 +0800

    1111

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,11 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        'admin_auth'=>\App\Http\Middleware\AdminAuth::class, //  自定义监测后台登录的中间件
+        'api_auth'=>\App\Http\Middleware\ApiAuth::class, //  自定义监测后台登录的中间件
+        'permission_auth'=>\App\Http\Middleware\PermissionAuth::class, // 自定义监测后台权限验证的中间件
+
+
     ];
 }
--- config/app.php
+++ config/app.php
@@ -121,7 +121,7 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    'log' => env('APP_LOG', 'daily'),
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
@@ -163,6 +163,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -225,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
